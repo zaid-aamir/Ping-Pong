a2141a18e0bbd6d9f8aa216c86ae0ab3d68bfdca
zaid-aamir <zaid.aamir@outlook.com>
2025-03-05 21:53:01 +0400

    Ball movement
    Added ball movement to the game and made sure that the ball bounces at the edge of the game.

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -1,18 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class BallMovement : MonoBehaviour
+public class BallController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 5f;
+    public Rigidbody2D rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(speed, Random.Range(-2f, 2f)).normalized * speed; // Randomize initial direction
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.gameObject.CompareTag("Bounce"))
+        {
+            // Get ball and paddle positions
+            float ballY = transform.position.y;
+            float paddleY = collision.transform.position.y;
+            float hitFactor = (ballY - paddleY) / (collision.collider.bounds.size.y / 2);
+
+            // Adjust X direction and add spin based on hit position
+            float newDirectionX = rb.velocity.x > 0 ? -1 : 1; // Flip X direction
+            Vector2 newVelocity = new Vector2(newDirectionX, hitFactor).normalized * speed;
+
+            rb.velocity = newVelocity;
+        }
+
+        //if(collision.gameObject.CompareTag())
     }
 }
